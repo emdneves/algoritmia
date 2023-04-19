0545e62e003bdb3ed8aee04a788ea78c7e5ea165
emdneves <ima7@live.com>
2023-04-19 22:18:48 +0100

    ex1 pl08

--- PL_08/PL_08_01.java
+++ PL_08/PL_08_01.java
@@ -1,17 +1,24 @@
 /* Escreva um programa que leia o conteúdo de um ficheiro de texto e imprima na consola.
  */
 
+ import java.io.File;
+ import java.io.IOException;
  import java.util.Scanner;
-
- class Main 
- {
-   public static void main(String[] args) 
-{
-float a = 3;
-int b = 2;
-
-System.out.println(a / b);
-
-}
-}
+ 
+ public class Main {
+     public static void main(String[] args) {
+         String fileName = "arquivo.txt"; // nome do arquivo
+         try {
+             File file = new File(fileName); // cria um objeto File com o nome do arquivo
+             Scanner scanner = new Scanner(file); // cria um objeto Scanner para ler o arquivo
+             while (scanner.hasNextLine()) { // lê linha por linha até o fim do arquivo
+                 String line = scanner.nextLine();
+                 System.out.println(line); // imprime a linha na console
+             }
+             scanner.close(); // fecha o scanner
+         } catch (IOException e) {
+             System.err.format("Erro na leitura do arquivo: %s%n", e);
+         }
+     }
+ }
  
