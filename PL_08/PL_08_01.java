cc4c484a2c417cd9637f9acc8f91b03548ff5fbf
emanuel <ima7@live.com>
2023-04-21 16:38:19 +0100

    untill ex 9 PL_08

--- PL_08/PL_08_01.java
+++ PL_08/PL_08_01.java
@@ -3,21 +3,27 @@
 
  import java.io.File;
  import java.io.IOException;
+ import java.io.PrintWriter;
  import java.util.Scanner;
  
- public class Main {
-     public static void main(String[] args) {
-         String fileName = "exercicio_01_Alternativa01.txt"; // nome do arquivo
-         try {
-             File file = new File(fileName); // cria um objeto File com o nome do arquivo
-             Scanner scanner = new Scanner(file); // cria um objeto Scanner para ler o arquivo
-             while (scanner.hasNextLine()) { // lê linha por linha até o fim do arquivo
+ public class PL_08_01 
+ {
+     public static void main(String[] args) 
+     {
+         String fileName = "exercicio_01_Alternativa01.txt"; 
+         try 
+         {
+             File file = new File(fileName); // creates an object File with the name of the file == name of the string fileName
+             Scanner scanner = new Scanner(file); // scanner to read the file
+             while (scanner.hasNextLine()) 
+             { // reads the content of the file, line by line, while there are lines to be read
                  String line = scanner.nextLine();
-                 System.out.println(line); // imprime a linha na console
+                 System.out.println(line); // prints file in the console, line by line
              }
-             scanner.close(); // fecha o scanner
-         } catch (IOException e) {
-             System.err.format("Erro na leitura do arquivo: %s%n", e);
+             scanner.close(); // closing the scanner
+         } catch (IOException erro) 
+         {
+             System.err.format("Erro na leitura do arquivo: %s%n", erro);
          }
      }
  }
--- PL_08/PL_08_07.java
+++ PL_08/PL_08_07.java
@@ -1,8 +1,50 @@
-/* Escreva um programa que leia o conteúdo de um ficheiro de texto e imprima o número de linhas e o 
+/* Escreva um programa que leia o conteúdo de um ficheiro de texto e imprima o número de lines e o 
 número de palavras presentes nesse ficheiro.  */
 
-package PL_08;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
-public class PL_08_07 {
-    
+public class PL_08_07 
+{
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            
+            Scanner in = new Scanner(new File("exercicio_07.txt"));
+            Scanner in2 = new Scanner(new File("exercicio_07.txt"));
+            int lines = 0;
+            int words = 0;
+
+
+            while (in.hasNextLine()) 
+
+            {
+                in.nextLine(); // reads the content of the file, line by line, while there are lines to be read
+                lines++;
+            }
+
+
+            while (in2.hasNext())
+                {
+                    in2.next();
+                    words++;
+                }
+
+            System.out.println("number of lines is: " + lines);
+            System.out.println("number of words is: " + words);
+
+            in.close();
+
+        } 
+        catch (IOException erro) 
+        {
+            System.err.format("Erro na leitura do arquivo: %s%n", erro);
+        }
+    }
 }
+
+
+
