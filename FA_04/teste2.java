bdded4bbb062c7b8cffa392da98f7f97781e7f2a
emanuel neves <ima7@live.com>
2023-05-01 21:30:20 +0100

    Update teste2.java

--- FA_04/teste2.java
+++ FA_04/teste2.java
@@ -1,121 +1,296 @@
 import java.io.File;
-import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+class Main {
 
-public class teste2 {
-    private static String[][] data;
-    private static int numRows;
-    private static int numColumns;
+    public static int lerFicheiro(File x) throws FileNotFoundException {
+       Scanner in = new Scanner(new File(x.toURI()));
 
-    public static void main(String[] args) {
-        readFile();
-        menu();
+       String linha = in.nextLine();
+       int sum=0;
+
+       while (in.hasNextLine()){
+
+           linha = in.nextLine();
+           sum++;
+
+      }
+
+        in.close();
+        return sum;
     }
 
-    private static void readFile() {
-        try {
-            File csvFile = new File("GameStart.csv");
-            Scanner scanner = new Scanner(csvFile);
+    public static String[][] splitFicheiro(File x) throws FileNotFoundException{
 
-            // Get number of rows and columns in the CSV file
-            numRows = 0;
-            numColumns = scanner.nextLine().split(";").length;
-            while (scanner.hasNextLine()) {
-                scanner.nextLine();
-                numRows++;
+        Scanner in = new Scanner(new File(x.toURI()));
+
+        int size = lerFicheiro(x);
+        String[] div = new String[9];
+
+        int i=0;
+
+        String linha = in.nextLine();
+        String[][] dados = new String[size][9];
+
+        while (in.hasNextLine()) {
+
+            linha = in.nextLine();
+            div = linha.split(";");
+
+            dados[i][0]=div[0];  // guarda idVenda
+            dados[i][1]=div[1];  // guarda idCliente
+            dados[i][2]=div[2];  // nomeCliente
+            dados[i][3]=div[3]; // contacto
+            dados[i][4]=div[4]; // email
+            dados[i][5]=div[5]; // editora
+            dados[i][6]=div[6]; // categoria
+            dados[i][7]=div[7]; // jogo
+            dados[i][8]=div[8]; // valor
+
+            i++;
+        }
+        return dados;
+    }
+    public static void print(String[][] dados) { //Op1
+
+        for (int i = 0; i < dados.length; i++) {
+            System.out.print("\n");
+
+            for (int x = 0; x < dados[0].length; x++) {
+                System.out.print(dados[i][x] + " ; ");
+
             }
-
-            // Create 2D array to store CSV data
-            data = new String[numRows][numColumns];
-
-            // Reset scanner to read from beginning of file
-            scanner = new Scanner(csvFile);
-
-            // Store CSV data in 2D array
-            for (int i = 0; i < numRows; i++) {
-                String[] line = scanner.nextLine().split(";");
-                for (int j = 0; j < numColumns; j++) {
-                    data[i][j] = line[j];
-                }
-            }
-
-            // Close scanner
-            scanner.close();
-        } catch (IOException e) {
-            System.out.println("Error reading file.");
         }
     }
+    public static double vendasFicheiro(String[][] x, boolean val){ //op2
 
-    private static void menu() {
-        Scanner scanner = new Scanner(System.in);
-        int choice = 0;
+        int sum = 0;
+        double num, total = 0;
 
-        while (choice != 7) {
-            System.out.println("\nSelect an option:");
-            System.out.println("1. Print the CSV file");
-            System.out.println("2. Print the number of sales and total revenue");
-            System.out.println("3. Calculate the total profit");
-            System.out.println("4. Print client information");
-            System.out.println("5. Print categories and games by editor");
-            System.out.println("6. Print the most expensive game and its clients");
-            System.out.println("7. Exit");
+            for (int j = 0; j < x.length; j++) {
+                num = Double.parseDouble(x[j][8]);
+
+                total += num;
+                sum++;
+            }
+        if (val) {
+            System.out.println("\nNumero total de vendas: " + sum);
+            System.out.printf("Valor Total : %,.2f€", total );
+        }
+        return total;
+    }
+
+    public static void lucro(double x){ //op3
+
+        double lucro = x * 0.1;
+        System.out.printf("O valor total de lucro é %,.2f€", lucro );
+
+    }
+
+    public static void infoCliente(String[][] x){
+        Scanner in = new Scanner(System.in);
+
+            boolean stop = false;
+
+        do {
 
             try {
-                choice = Integer.parseInt(scanner.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input.");
-                continue;
+                int id=0, i = 0;
+                System.out.print("\nPor favor, introduza Id do cliente: ");
+
+                int op = in.nextInt();
+
+
+                while (!stop && i < x.length) {
+
+                    id = Integer.parseInt(x[i][1]);
+
+                    if (id == op) {
+
+                        System.out.println("\nNome: " + x[i][2] + "\nContacto: " + x[i][3] + "\nEmail: " + x[i][4]);
+
+                        stop = true;
+                    }
+
+                    i++;
+                }
+
+                if (!stop){
+                    System.out.println("Id desconhecido");
+                }
+
+            } catch (InputMismatchException exc) {
+                System.out.println("Id inválido, tem de ser numérico!");
+                in.next();
             }
 
-            switch (choice) {
-                case 1:
-                    printCsv();
-                    break;
-                case 2:
-                    printSalesInfo();
-                    break;
-/*                 case 3:
-                    printTotalProfit();
-                    break;
-                case 4:
-                    printClientInfo();
-                    break;
-                case 5:
-                    printCategoriesAndGames();
-                    break;
-                case 6:
-                    printMostExpensiveGame();
-                    break; */
-                case 7:
-                    System.out.println("Goodbye!");
-                    break;
-                default:
-                    System.out.println("Invalid input.");
-                    break;
-            }
-        }
+        }while (!stop);
+    }
+    public static void editora(String[][] x) { //op5
 
-        scanner.close();
+        Scanner input = new Scanner(System.in);
+
+        String genero = "", jogo ="", resp, editora;
+        String[] categorias = new String[10];
+        String[] jogos = new String[10];
+        boolean verEdi= false;
+
+        do {
+
+            System.out.print("\nInsira a editora a consultar: ");
+            editora = input.nextLine();
+
+            System.out.println("\n---Categorias e Jogos da " + editora + "---");
+
+            for (int i = 0; i < x.length; i++) {
+
+                if (x[i][5].equals(editora)) {
+                    genero = x[i][6];
+                    jogo = x[i][7];
+                    verEdi = true;
+
+                    boolean verCateg = false;
+                    int j = 0;
+
+                    while (!verCateg) {
+
+                        if (categorias[j] == null) {
+                            categorias[j] = genero;
+                            System.out.println("\n" + categorias[j] + ":");
+
+                        } else if (categorias[j].equals(genero)) {
+                            verCateg = true;
+
+                        } else {
+                            j++;
+                        }
+                    }
+                    int k = 0;
+                    boolean verJog = false;
+
+                    while (!verJog) {
+
+                        if (jogos[k] == null) {
+                            jogos[k] = jogo;
+                            System.out.println(jogo);
+
+                        } else if (jogos[k].equals(jogo)) {
+                            verJog = true;
+
+                        } else {
+                            k++;
+                        }
+                    }
+                }
+            }
+            if (!verEdi) {
+                System.out.println("\nEssa Editora não está no Sistema ou tem erro Ortográfico!");
+                System.out.println("Deseja tentar de novo? (s/n)");
+                resp = input.next();
+
+                if (resp.equals("s")){
+
+                    input.nextLine();
+
+                } else {
+                    verEdi = true;
+                }
+            }
+        }while (!verEdi);
     }
 
-    private static void printCsv() {
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numColumns; j++) {
-                System.out.print(data[i][j] + "\t");
+
+    public static void jogoMaiorValor(String[][] x) { //op6
+        double maior = 0;
+
+        System.out.println("\nAs vendas mais caras foram: ");
+        for (int j = 0; j < x.length; j++) {
+            double valor = Double.parseDouble(x[j][8]);
+            if (valor > maior) {
+                maior = valor;
             }
-            System.out.println();
         }
+
+        for (int i = 0; i < x.length; i++) {
+            double comp = Double.parseDouble(x[i][8]);
+            String nome = x[i][2], jogo = x[i][7];
+            if (comp == maior) {
+                System.out.println(jogo + " - " + maior + "€ | comprado por: " + nome);
+            }
+        }
+
     }
+    public static void menu(File x) throws FileNotFoundException {
+            Scanner in = new Scanner(new File(x.toURI()));
+            Scanner input = new Scanner(System.in);
 
-    private static void printSalesInfo() {
-        int numSales = numRows - 1;
-        double totalRevenue = 0.0;
+            int opcao=0;
+            String[][] dados = splitFicheiro(x);
 
-        for (int i = 1; i < numRows; i++) {
-            totalRevenue += Double.parseDouble(data[i][numColumns - 1]);
+            do {
+
+                System.out.println("\n\nPor favor, escolha a opção pretendida: \n");
+                System.out.println("1 - Imprimir ficheiro\n2 - Quantidade e Total de vendas\n3 - Lucros\n4 - Ver dados de Cliente\n5 - Generos e Jogos da editora\n6 - Ver jogo mais caro\n7 - Sair");
+                System.out.print("\n-> ");
+
+                try {
+
+                    opcao = input.nextInt();
+
+                    switch (opcao){
+
+                        case 1: print(dados);
+                        break;
+
+                        case 2: vendasFicheiro(dados, true);
+
+                        break;
+
+                        case 3: lucro(vendasFicheiro(dados, false));
+                        break;
+
+                        case 4: infoCliente(dados);
+                            break;
+
+                        case 5: editora(dados);
+                            break;
+
+                        case 6: jogoMaiorValor(dados);
+                            break;
+
+                        case 7: break;
+
+                        default:
+                            System.out.println("Opção inválida!");
+                    }
+
+                } catch (InputMismatchException exc) {
+
+                    System.out.println("Opção inválida!");
+                    input.next();
+
+                }
+
+            } while (opcao != 7);
+
+            in.close();
+
         }
 
-        System.out.printf("Number of sales: %d\n", numSales);
-        System.out.printf("Total revenue: %.2f\n", totalRevenue);
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        try {
+            File ficheiro = new File("GameStart.csv");
+            menu(ficheiro);
+
+        } catch (FileNotFoundException exc){
+
+            System.out.print("Not found");
+        }
+
     }
 }
