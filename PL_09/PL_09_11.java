6adfe0700f6a09a0c5ff1f67174ff7987ebef957
emanuel <ima7@live.com>
2023-05-05 09:50:50 +0100

    ex 11 PL_09

--- PL_09/PL_09_11.java
+++ PL_09/PL_09_11.java
@@ -7,8 +7,24 @@ No método main, crie um objeto dessa classe e chame os métodos para depositar 
 Em seguida, crie dois objetos dessa classe e atribua valores aos seus atributos. 
 Em seguida, transfira um valor de uma conta, deve antes confirmar se a transferência é válida, e executar, para a outra e exiba o saldo de cada uma na tela.
  */
-package PL_09;
+import java.io.PrintStream;
 
-public class PL_09_11 {
-    
+public class Ex_11 {
+    public Ex_11() {
+    }
+
+    public static void main(String[] args) {
+        Conta conta1 = new Conta("111222", "Cristiano Ronaldo", 120.0);
+        Conta conta2 = new Conta("222333", "Conor Mcgregor", 400.0);
+        conta1.transferencia(130.0, conta2);
+        conta2.transferencia(200.0, conta1);
+        PrintStream var10000 = System.out;
+        String var10001 = conta1.getNumeroConta();
+        var10000.println("Saldo da Conta " + var10001 + ": " + conta1.getSaldo());
+        var10000 = System.out;
+        var10001 = conta2.getNumeroConta();
+        var10000.println("Saldo da Conta " + var10001 + ": " + conta2.getSaldo());
+    }
 }
+
+
